556d9f31957d8a63b211aa4c7bc3217102907fc7
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-07-09 02:29:34 +0300

    Header: Done for Tablet, PC and Mob wait fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
-//Add and remove class Active
+// Navbar: class Active and hr element
 $(document).ready(function(){
+	$("li > a").first().append('<hr class="hrNav">');
+
     $("a").on("click", switchActive());
 
     function switchActive() {
-	$(".nav > li > a").click(function() {
-		    $(this).parent().addClass('active')
-		    	.siblings().removeClass('active');
-	    });
+		$(".nav > li > a").click(function() {
+			$(this).parent().addClass('active')
+				.siblings().removeClass('active');
+
+			$(".hrNav").remove();
+			$(this).append('<hr class="hrNav">');
+		});
 	}
 });
